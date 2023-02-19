bcde698e780d90030866d7f320c41ec068e57a45
Alena18 <sun180983@gmail.com>
2023-02-19 16:00:00 +0000

    Make request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const API_KEY = "3X3PMnklqc66kDV2zAIbqvMSpWo";
-const API_URL = "https://ci-jshint.herokuapp.com/api?api_key";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
@@ -13,6 +13,19 @@ async function getStatus(e) {
    const data = await response.json();
 
    if (response.ok) {
-      console.log(data);
-   };
+      //console.log(data);  // or data.expire to see expiry date
+      displayStatus(data);
+   } else {
+      throw new Error(data.error);
+   }
 };
+
+function displayStatus(data) {
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
