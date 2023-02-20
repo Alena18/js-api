3daa81d5cf365a878ed843ab42743e5d390e49fa
Alena18 <sun180983@gmail.com>
2023-02-20 12:49:44 +0000

    Add post form to send api request

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,25 @@ const API_KEY = "3X3PMnklqc66kDV2zAIbqvMSpWo";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
+// Get status button
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+// Wire up our Run Checks button
+document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+// Create postForm
+async function postForm(e) {
+
+   const form = new FormData(document.getElementById("checksform"));
+
+   const response = await fetch(API_URL, {
+       method: "POST",
+       headers: {
+           "Authorization": API_KEY,
+       },
+       body: form,
+   });
+
+}
 // Create a promise
 async function getStatus(e) {
    const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -29,3 +46,4 @@ function displayStatus(data) {
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
 }
+
